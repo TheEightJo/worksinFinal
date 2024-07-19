87f892b1782fc4a56d46bbafc94967058f10dc3a
seokho66 <ssh970401@naver.com>
2024-07-19 12:45:53 +0900

    7/19

--- worksIn/src/main/java/com/WorksIn/entity/BaseTimeEntity.java
+++ worksIn/src/main/java/com/WorksIn/entity/BaseTimeEntity.java
@@ -1,5 +1,7 @@
 package com.WorksIn.entity;
 
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -10,6 +12,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -18,9 +21,11 @@ import java.time.LocalDateTime;
 @MappedSuperclass // 부모 클래스를 상속받는 자식 클래스에 매핑정보만 제공
 @Data
 public abstract class BaseTimeEntity {
+
     @CreatedDate // 생성시 자동 저장
     @Column(updatable = false)
     private LocalDateTime regTime; // 등록일
+
 
     @LastModifiedDate // 변경시 자동 저장
     private LocalDateTime updateTime; // 수정일
